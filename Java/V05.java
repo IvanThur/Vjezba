3d64f9079d2b963d0e90a201ed5be27cb2f91bfd
IvanThur <93528066+IvanThur@users.noreply.github.com>
2021-12-23 17:12:47 +0100

    Project Euler 4.zadatak

---
+++ Java/V05.java
@@ -0,0 +1,19 @@
+package vjezba;
+
+public class VO5 {
+	public static void main(String[] args) {
+		
+		for(int i=1;;i++) {
+			for(int j=1;j<=20;j++) {
+			if(i%j!=0) {
+				break;
+			}
+			if(j==20) {
+				System.out.println(i);
+				break;
+			}
+		}}
+		
+	}
+
+}
