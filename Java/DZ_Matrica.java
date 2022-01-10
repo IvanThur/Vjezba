8f841862557341012dd31d468082eef632d5050d
IvanThur <93528066+IvanThur@users.noreply.github.com>
2022-01-10 09:32:32 +0100

    Izmjena imena varijabli i brisanje nepotrebnih

--- Java/DZ_Matrica.java
+++ Java/DZ_Matrica.java
@@ -5,50 +5,52 @@ import javax.swing.JOptionPane;
 public class DZ_Matrica {
 	public static void main(String[] args) {
 
-		int n = 6, i, j , k = 0, stupac = 0, red = n-1,b,c,d;
+		int n, i, j , k = 0, stupac = 0, red,b,redmax,redmin,m;
 
-		n=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova i stupaca"));
-		int niz[]=new int[n*n];
+		n=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
+		m=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
+		int niz[]=new int[n*m];
 		
 		
-		for(i=0;i<n*n;i++) {
-			niz[i]=Integer.parseInt(JOptionPane.showInputDialog("Unesi "+(i+1)+" broj niza"));
+		for(i=0;i<n*m;i++) {
+			//niz[i]=Integer.parseInt(JOptionPane.showInputDialog("Unesi "+(i+1)+" broj niza"));
+			niz[i]=i+1;
 		}
 		
-		int mat[][] = new int[n][n];
+		int mat[][] = new int[n][m];
 
-			c=n-1;
-			stupac = n-1;
+			redmax=n-1;
+			stupac = m-1;
 			red=n-1;
-			d=0;
+			redmin=0;
 		for(b=(n-(n/2));b>0;b--) {
-			for (j = stupac; j >= d; j--) {
-				mat[c][j] = niz[k++];
+			for (j = stupac; j >= redmin; j--) {
+				mat[redmax][j] = niz[k++];
 				stupac = j;				
 			}
 			
 			red=red-1;
-			for (i = red; i >=d; i--) {
+			for (i = red; i >=redmin; i--) {
 				mat[i][stupac] = niz[k++];
 				red = i;
 			}
 			stupac=stupac+1;
-			for (j =stupac; j <= c; j++) {
-				mat[d][j] = niz[k++];
+			for (j =stupac; j <= redmax; j++) {
+				mat[redmin][j] = niz[k++];
 				stupac = j;
 			}
 			red+=1;
 			
-			for (i = red; i < c; i++) {
+			for (i = red; i < redmax; i++) {
 				mat[i][stupac] = niz[k++];
 				red=i;
 			}
 			stupac--;
-			c--;
-			d++;
+			redmax--;
+			redmin++;
 	}
 		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
+			for (j = 0; j < m; j++) {
 				System.out.print("\t"+ mat[i][j]);
 			}
 			System.out.println();
